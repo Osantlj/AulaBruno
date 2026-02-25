001e0c14f3a0ee9ac8037cd8beeccf2d5855e782
Osantlj <senac12938@gmail.com>
2026-02-24 21:33:21 -0300

    24/02

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 import app from "./src/app.js";
+import conexao from "./infra/conexao.js";
 
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+//realizando a conexao
+conexao.connect((erro) => {
+
+    if (erro) {
+        console.log("Erro na conexão")
+    } else {
+        console.log("Conectado com sucesso")
+        //escutando a porta
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`)
+        })
+    }
 });
+
+
+
+
